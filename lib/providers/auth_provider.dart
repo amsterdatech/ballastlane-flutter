fefecfd1c907cfdc3632709c68e7c3f2c5c0aae9
William Gouvea <williamg@backbase.com>
2024-08-15 17:33:25 -0300

    Add login method to auth_provider.dart

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,9 +1,17 @@
-
-
 import 'package:flutter/material.dart';
 
-class AuthProvider with ChangeNotifier{
+class AuthProvider with ChangeNotifier {
   bool _isAuthenticated = false;
 
   bool get isAuthenticated => _isAuthenticated;
+
+  Future<void> login(String username, String password) async {
+    if (username == 'admin' && password == 'admin') {
+      _isAuthenticated = true;
+      notifyListeners();
+    } else {
+      _isAuthenticated = false;
+      notifyListeners();
+    }
+  }
 }
--- test/providers/auth_provider_test.dart
+++ test/providers/auth_provider_test.dart
@@ -6,6 +6,17 @@ void main() {
   test('Initial auth state should be unauthenticated', () {
     final authProvider = AuthProvider();
     expect(authProvider.isAuthenticated, false);
+  });
 
+  test('Login with correct credentials should autenticate the user', () async {
+    final authProvider = AuthProvider();
+    await authProvider.login('admin','admin');
+    expect(authProvider.isAuthenticated, true);
+  });
+
+  test('Login with incorrect credentials should not authenticate user', () async {
+    final authProvider = AuthProvider();
+    await authProvider.login('user', 'user');
+    expect(authProvider.isAuthenticated, false);
   });
 }
